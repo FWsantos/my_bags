75e73e86784a16affcccd327c42e8a4b71b13ddd
FWsantos <fwsantos23@gmail.com>
2024-07-04 11:34:32 -0300

    migrate to js

---
+++ prisma/seed.js
@@ -0,0 +1,39 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+const productData = [
+  {
+      name: 'Estojo',
+      description: 'Estojo escolar',
+      price: 6.00,
+      image: 'https://www.google.com.br',
+    },
+    {
+      name: 'Mochila',
+      description: 'Mochila escolar',
+      price: 50.00,
+      image: 'https://www.bing.com.br',
+  },
+]
+
+async function main() {
+  console.log(`Start seeding ...`)
+  for (const p of productData) {
+    const product = await prisma.product.create({
+      data: p,
+    })
+    console.log(`Created product with id: ${product.id}`)
+  }
+  console.log(`Seeding finished.`)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
